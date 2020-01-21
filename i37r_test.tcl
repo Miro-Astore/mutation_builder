6f4e3e115faa1d25d321a0920047f259c6121e12
Miro-Astore <miro.astore@gmail.com>
2020-01-21 13:48:53 +1100

    fixed ion renumbering

--- i37r_test.tcl
+++ i37r_test.tcl
@@ -4,6 +4,7 @@
 #make directory for temporary files if it doesn't exist
 
 file mkdir build_files
+file mkdir build_files/ions
 
 #mol new ../final_wt_ionized_added_loops_no_ters_cter_modelled_use_this_gmx_ready.psf
 #mol addfile ../final_wt_ionized_added_loops_no_ters_cter_modelled_use_this_gmx_ready.pdb
@@ -44,7 +45,7 @@ topology /home/miro/Downloads/toppar/top_all36_prot.rtf
 pdbalias atom ILE CD1 CD 
 
 #readpsf build_files/AP1.psf
-coordpdb build_files/AP1.pdb
+#coordpdb build_files/AP1.pdb
 segment AP1 { 
 	pdb build_files/AP1.pdb
 	first NONE
@@ -54,7 +55,7 @@ coordpdb build_files/AP1.pdb AP1
 
 
 #readpsf build_files/BP1.psf
-coordpdb build_files/BP1.pdb
+#coordpdb build_files/BP1.pdb
 segment BP1 { 
 	pdb build_files/BP1.pdb
 	first NONE
@@ -93,24 +94,62 @@ autoionize -psf build_files/mutated_system.psf -pdb build_files/mutated_system.p
 #
 
 #now we have to re order everything so gromacs doesn't complain that our index groups aren't contiguous
+#we need to renumber the residues as well else they will still be out of order
 mol delete all
 mol new build_files/ionized.psf 
 mol addfile build_files/ionized.pdb
-set sel [atomselect top "ion"]
-#set names [$sel get name]
-#set names [ lsort -unique $names ]
-#foreach name $names {
-#
-#	set ionsel [atomselect top "ion and name $name"] 
-#	$ionsel writepsf build_files/ions/$name.psf
-#	$ionsel writepdb build_files/ions/$name.pdb
-#}
-$sel writepsf build_files/ions.psf
-$sel writepdb build_files/ions.pdb
+set sel [atomselect top "segname \"ION.*\""]
+set names [$sel get name]
+set names [ lsort -unique $names ]
+set sel_not_ion [atomselect top "not segname \"ION.*\""]
+$sel_not_ion writepsf build_files/not_ions.psf
+$sel_not_ion writepdb build_files/not_ions.pdb
+puts $names
+set seed_id 1
+foreach name $names {
+	puts $name
+	mol delete all 
+	mol new build_files/ionized.psf 
+	mol addfile build_files/ionized.pdb
+	set ionsel [atomselect top "segname \"ION.*\" and name $name"] 
+	$ionsel set segname ION
+	$ionsel writepsf build_files/ions/$name.psf
+	$ionsel writepdb build_files/ions/$name.pdb
+	mol delete all 
+	mol new build_files/ions/$name.psf 
+	mol addfile build_files/ions/$name.pdb 
+	set sel [atomselect top "all"]
+	set residue_list [$sel get residue] 
 
-set sel [atomselect top "not ion"]
-$sel writepsf build_files/not_ions.psf
-$sel writepdb build_files/not_ions.pdb
+	#we need to get the starting point for the resid numbers else we will have overlaps and psfgen really doesn't like that 
+	#we need to start from index 1 nad not 0 because autoionize makes us start from 1 , this way we get the first non 1 element 
+	#ok none of that worked just number them from 1 and up 
+	#might want the magnesiums to be of a different segment but we'll see how we go 
+	#better plan is to keep them separate so we adjust teh selection test to be 'ION.*'
+#	set seed_id [ $sel get resid ]
+#	set seed_id [lsort -unique $seed_id]
+#	set seed_id [lsort -integer $seed_id]
+#	set seed_id [lindex $seed_id 1 ]
+
+	foreach id $residue_list { 
+		set sel_curr [atomselect top "residue $id"]
+		#$sel_curr set resid [expr $id + $seed_id]
+		$sel_curr set resid $seed_id
+		set seed_id [ expr $seed_id + 1 ]
+	}
+	set sel [atomselect top "ion and name $name"]
+	$sel writepdb build_files/ions/$name.pdb
+	$sel writepsf build_files/ions/$name.psf
+}
+resetpsf
+
+foreach name $names {
+	readpsf build_files/ions/$name.psf
+	coordpdb build_files/ions/$name.pdb 
+}
+writepsf build_files/ions.psf
+writepdb build_files/ions.pdb
+
 resetpsf 
  
 #re merging ions and non ions
